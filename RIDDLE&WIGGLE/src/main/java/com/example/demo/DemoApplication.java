ad23a5cc6f21202a7c031b37517bb392ce50fd52
Jaco <jacox0016@gmail.com>
2024-04-22 13:56:36 +0200

    Parte servidor
    ^_^

--- RIDDLE&WIGGLE/src/main/java/com/example/demo/DemoApplication.java
+++ RIDDLE&WIGGLE/src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,34 @@
 package com.example.demo;
 
+import java.util.Objects;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.lang.NonNull;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
 
 @SpringBootApplication
-public class DemoApplication {
+@EnableWebSocket
+
+
+public class DemoApplication implements WebSocketConfigurer {
 	
+
+	@Override
+	public void registerWebSocketHandlers(@NonNull WebSocketHandlerRegistry registry) {
+		registry.addHandler(Objects.requireNonNull(appHandler()), "/app")
+			.setAllowedOrigins("*");
+	}
+	
+	@Bean
+	public WebSocketAppHandler appHandler(){
+		return new WebSocketAppHandler();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
--- RIDDLE&WIGGLE/src/main/java/com/example/demo/WebSocketAppHandler.java
+++ RIDDLE&WIGGLE/src/main/java/com/example/demo/WebSocketAppHandler.java
@@ -1,5 +1,107 @@
 package com.example.demo;
 
-public class WebSocketAppHandler {
-    
+//import java.util.Map;
+import java.util.Objects;
+//import java.util.concurrent.ConcurrentHashMap;
+
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+//import org.springframework.web.socket.WebSocketExtension;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+import org.springframework.lang.NonNull;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+
+
+
+public class WebSocketAppHandler extends TextWebSocketHandler {
+
+    private ObjectMapper mapper = new ObjectMapper();
+    private WebSocketSession sesionRiddle;
+	private WebSocketSession sesionWiggle;
+
+    @Override
+	public void afterConnectionEstablished(@NonNull WebSocketSession session) throws Exception {
+		System.out.println("Nuevo usuario: " + session.getId());
+
+		//Asignamos uan sesion a cada jugador 
+		if(sesionRiddle == null){
+
+			sesionRiddle = session;
+
+
+		}else if(sesionWiggle == null){
+
+			sesionWiggle = session;
+
+
+		}else{
+			System.out.println("Numero maximo de jugadores alcanzado.");
+		}
+		
+	}
+
+    @Override
+	public void afterConnectionClosed(@NonNull WebSocketSession session, @NonNull CloseStatus status) throws Exception {
+		System.out.println("Sesion cerrada: " + session.getId());
+		if(session == sesionRiddle) {sesionRiddle = null;}
+		else if ( session == sesionWiggle) {sesionWiggle = null;}
+		
+	}
+
+    @Override
+	protected void handleTextMessage(@NonNull WebSocketSession session, @NonNull TextMessage msg) throws Exception {
+		
+		System.out.println("Mensage recivido: " + msg.getPayload());
+		JsonNode message = mapper.readTree(msg.getPayload()); // Convierte en JSON.
+
+		String requestType = message.get("tipo").asText();
+		String contentNode = message.get("contenido").toString();
+		
+		if(session == sesionRiddle){
+			
+			if(sesionWiggle != null){
+				EnviarMensaje(
+                    sesionWiggle, 
+				requestType, 
+				contentNode
+				);
+			}
+
+		} else if (session == sesionWiggle){
+
+			EnviarMensaje(
+				sesionRiddle, 
+				requestType, 
+				contentNode
+			);
+
+		}else{
+			System.out.println("Una sesion desconocida esta intantado mandar informacion.");
+		}
+		
+	}
+
+    private void EnviarMensaje(
+		WebSocketSession session, 
+		String type, 
+		String content) throws Exception {
+			
+		session.sendMessage(
+		new TextMessage(buildStringedJSON(type, content)));
+	}
+
+    private @NonNull String buildStringedJSON(String type, String content){ // construye un JSON y devuelve un String
+		ObjectNode jsonNode = mapper.createObjectNode();
+        jsonNode.put("tipo", type);
+        jsonNode.put("contenido", content);
+		return Objects.requireNonNull(jsonNode.toString());
+	}
+
+
+
 }
